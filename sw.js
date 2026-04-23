6c7f6cee212dc616bcdd17487df9b332363a0c8f
ismailarbudu-cpu <ismailarbudu@gmail.com>
2026-04-23 07:05:58 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'hifz-trainer-v1';
+const ASSETS = [
+  'quran_trainer_2.html',
+  'sw.js',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+// Установка Service Worker и кэширование базовых файлов
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Работа с запросами (позволяет открывать оболочку приложения без сети)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
